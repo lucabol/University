bb4f63643fe70b2cd97a07823a71f0a789a71162
Luca Bolognese <lucabol@microsoft.com>
2017-02-28 14:24:42 +0100

    Fixed batch file for demo

--- Utils/Cleanup.bat
+++ Utils/Cleanup.bat
@@ -1,7 +1,7 @@
 @echo off
 cd ..\Class
-powershell .\CleanupVMs.ps1 -LabName Stats
-powershell .\CleanupVMs.ps1 -LabName Accounting
-powershell .\CleanupVMs.ps1 -LabName Physics
-powershell .\CleanupVMs.ps1 -LabName Management
+start powershell .\CleanupVMs.ps1 -LabName Stats
+start powershell .\CleanupVMs.ps1 -LabName Accounting
+start powershell .\CleanupVMs.ps1 -LabName Physics
+start powershell .\CleanupVMs.ps1 -LabName Management
   
