9555f4a156398215580aceceef86b98b304e69be
Luca Bolognese <lucabol@microsoft.com>
2017-03-29 20:34:58 +0200

    Delete obsolete scripts

--- Utils/Cleanup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd ..\Class
-start powershell .\CleanupVMs.ps1 -LabName Stats
-start powershell .\CleanupVMs.ps1 -LabName Accounting
-start powershell .\CleanupVMs.ps1 -LabName Physics
-start powershell .\CleanupVMs.ps1 -LabName Management
-  
