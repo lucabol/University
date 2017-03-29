adf52d0b67eda347add535434cfa8b0dcac76343
Mirko Napolano <mirkon@microsoft.com>
2017-03-29 23:46:46 +0200

    script for changing task from storage account

---
+++ Src/Shutdown scripts/changeTaskIdle_remote.ps1
@@ -0,0 +1,9 @@
+﻿#s$ErrorActionPreference = "Stop"
+
+$info = Get-AzureRmStorageAccount -Name backupbocconiimagelab -ResourceGroupName backupVLAB | Get-AzureStorageContainer | Get-AzureStorageBlob -Blob Shutdown*
+
+Get-AzureStorageBlobContent -Container upload -Blob $info.Name -Destination "C:\" -Context $info.Context -Force -ErrorAction Stop
+
+schtasks.exe /delete /TN "Shutdown on idle" /f
+
+schtasks.exe /create /TN "Shutdown on idle" /XML "C:\Shutdown on idle.xml"
