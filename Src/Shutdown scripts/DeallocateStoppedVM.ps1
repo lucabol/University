afe6caeb1d9bbfb0c53f2e288cb1b0d96537fd51
Mirko Napolano <mirkon@microsoft.com>
2017-04-25 20:34:23 +0200

    optimization in finding compute vm for labVM; adding the possibility to launch it from local machine

--- Src/Shutdown scripts/DeallocateStoppedVM.ps1
+++ Src/Shutdown scripts/DeallocateStoppedVM.ps1
@@ -1,100 +1,91 @@
 ﻿Param
 (
- # Lab Name
-[Parameter(Mandatory=$true, HelpMessage="Name of Lab")]
-[string] $LabName
+     # Lab Name
+    [Parameter(Mandatory=$true, HelpMessage="Name of Lab")]
+    [string] $LabName,
+
+    [Parameter(Mandatory=$false, HelpMessage="Path to file with Azure Profile")]
+    [string] $profilePath = "$env:APPDATA\AzProfile.txt"
 )
+
+
+function LoadAzureCredentials {
+    [CmdletBinding()]
+    param($credentialsKind, $profilePath)
+
+    Write-Verbose "Credentials Kind: $credentialsKind"
+    Write-Verbose "Credentials File: $profilePath"
+
+    if(($credentialsKind -ne "File") -and ($credentialsKind -ne "RunBook")) {
+        throw "CredentialsKind must be either 'File' or 'RunBook'. It was $credentialsKind instead"
+    }
+
+    if($credentialsKind -eq "File") {
+        if (! (Test-Path $profilePath)) {
+            throw "Profile file(s) not found at $profilePath. Exiting script..."    
+        }
+        Select-AzureRmProfile -Path $profilePath | Out-Null
+    } else {
+        $connectionName = "AzureRunAsConnection"
+        $SubId = Get-AutomationVariable -Name 'SubscriptionId'
+
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+        
+        Set-AzureRmContext -SubscriptionId $SubId                            
+    } 
+}
 
 $VerbosePreference = "continue"
 
-$connectionName = "AzureRunAsConnection"
-try
-{
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
 
-    "Logging in to Azure..."
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+if($PSPrivateMetadata.JobId) {
+        $credentialsKind = "Runbook"
+        $ShutdownPath = Get-AutomationVariable -Name 'ShutdownPath'
+        $VNetName = Get-AutomationVariable -Name 'VNetName'
+        $SubnetName = Get-AutomationVariable -Name 'SubnetName'
+        $Size = Get-AutomationVariable -Name 'Size'   
 }
-catch 
-{
-    if (!$servicePrincipalConnection)
-    {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else{
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
+else {
+    $credentialsKind =  "File"
 }
 
-#we have to find the RG of the compute VM (different from the RG of the lab and the labVM)
+LoadAzureCredentials -credentialsKind $credentialsKind -profilePath $profilePath
+
+#we have to find the RG of the compute VM, which is different from the RG of the lab and the labVM
 
 #get the RG of the lab
 $ResourceGroupName = (Find-AzureRmResource -ResourceType "Microsoft.DevTestLab/labs" -ResourceNameContains $LabName).ResourceGroupName
 
-#get the lab VMs
-$labVM=Find-AzureRmResource -ResourceType "Microsoft.DevTestLab/labs/virtualMachines" -ResourceGroupName $ResourceGroupName
+
+#get the expanded properties of the VMs
+$labVMs = Find-AzureRmResource -ResourceType 'Microsoft.DevTestLab/labs/virtualmachines' -ResourceNameContains "$LabName/" -ExpandProperties | ? { $_.ResourceName -like "$LabName/*" }
 
 
-#get the resourceID of each labVM
-$labVmId = @()
-foreach ($machine in $labVM){
-    $labVmId += $machine.ResourceId
-}
-
-
-$VirtualMachines = @()
-foreach ($id in $labVmId){
-    #get the specific compute id from the labVmId
-    $computeId = (Get-AzureRmResource -Id $id).Properties.computeId
+foreach ($vm in $labVMs){
 
     #get the actual RG of the compute VM
-    $labVmRGName=(Get-AzureRmResource -Id $computeId).resourcegroupname
+    $labVmRGName=(Get-AzureRmResource -Id $vm.Properties.computeid).ResourceGroupName
 
-    #Get all the compute VMs of the specific compute RG. Avoid recalling for the same RG
-    if ($VirtualMachines.ResourceGroupName -notcontains $labVmRGName){
-        $VirtualMachines += Get-AzureRmVM -ResourceGroupName $labVmRGName
-    }
-}
-
-
-
-foreach ($VM in $VirtualMachines)
-{
-    #get the VM status
-	#PowerState/deallocated = Stopped(deallocated)
-	#PowerState/stopped = Stopped (shutdown from the OS)
     $VMStatus = (Get-AzureRmVM -ResourceGroupName $labVmRGName -Name $VM.Name -Status).Statuses.Code[1]
-    
-            Write-Verbose ("Status VM  "+ $VM.Name + " :" + $VMStatus)
+
+    Write-Verbose ("Status VM  "+ $VM.Name + " :" + $VMStatus)
             
-            if ($VMStatus -eq 'PowerState/deallocated')
-            {
-            Write-Output ($VM.Name + " is already deallocated")
-            }
+    if ($VMStatus -eq 'PowerState/deallocated')
+    {
+        Write-Output ($VM.Name + " is already deallocated")
+    }
 
-            elseIf ($VMStatus -eq 'PowerState/stopped')
-            {
-                #force the VM deallocation
-                $stopping = Stop-AzureRmVM -Name $VM.Name -ResourceGroupName $labVmRGName -Force
+    elseIf ($VMStatus -eq 'PowerState/stopped')
+    {
+        #force the VM deallocation
+        Stop-AzureRmVM -Name $VM.Name -ResourceGroupName $labVmRGName -Force
 
-                #check again the VM status
-                $VMStatus = (Get-AzureRmVM -ResourceGroupName $labVmRGName -Name $VM.Name -Status).Statuses.Code[1]
+    }
 
-                    if ($VMstatus -eq 'PowerState/deallocated')
-                        
-                    {
-                        Write-Output ($VM.Name + " deallocated")
-                        }
-
-                    else
-                    {
-                        Write-Output ($VM.Name + " fail to deallocate")
-                    }
-            }
 }
